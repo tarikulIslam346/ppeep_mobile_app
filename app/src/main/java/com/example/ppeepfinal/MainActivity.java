7edbcff109ab1d80bbbda1ac0f3f9626aeeab1d2
System Administrator <root@MacBooks-MacBook-Air.local>
2019-06-12 13:36:32 +0600

    switch login page

--- app/src/main/java/com/example/ppeepfinal/MainActivity.java
+++ app/src/main/java/com/example/ppeepfinal/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.ppeepfinal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_input_screen);
+
+        loginButton = (Button) findViewById(R.id.user_log_in);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(MainActivity.this,LogInPage.class);
+                startActivity(loginIntent);
+            }
+        });
+
     }
 }
