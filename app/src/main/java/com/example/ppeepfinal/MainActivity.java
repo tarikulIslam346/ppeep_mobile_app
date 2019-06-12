253bc72a54c6a08947a176e12b9558ecf16d88a3
P-PeeP <nayeemhasnat00@gmail.com>
2019-06-12 13:42:07 +0600

    login intent nayeem

--- app/src/main/java/com/example/ppeepfinal/MainActivity.java
+++ app/src/main/java/com/example/ppeepfinal/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.ppeepfinal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button LogInButton, SignUpButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_input_screen);
+        LogInButton= (Button) findViewById(R.id.user_log_in);
+        LogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent=new Intent(MainActivity.this,LogInPage.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 }
