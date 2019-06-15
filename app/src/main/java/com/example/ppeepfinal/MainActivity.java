428a3f6f8a58769e10f06455bb5e56560a775ac0
System Administrator <root@MacBooks-MacBook-Air.local>
2019-06-15 18:21:29 +0600

    Facebook OTP + home page

--- app/src/main/java/com/example/ppeepfinal/MainActivity.java
+++ app/src/main/java/com/example/ppeepfinal/MainActivity.java
@@ -6,7 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.accountkit.AccessToken;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitLoginResult;
 import com.facebook.accountkit.ui.AccountKitActivity;
 import com.facebook.accountkit.ui.AccountKitConfiguration;
 import com.facebook.accountkit.ui.LoginType;
@@ -29,35 +33,58 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 phoneLogin(v);
-               /* Intent letsGoIntent = new Intent(MainActivity.this,OTPpage.class);
-                startActivity(letsGoIntent);*/
-            }
-            public void phoneLogin(final View view) {
-                final Intent intent = new Intent(MainActivity.this, AccountKitActivity.class);
-                AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
-                        new AccountKitConfiguration.AccountKitConfigurationBuilder(
-                                LoginType.PHONE,
-                                AccountKitActivity.ResponseType.CODE); // or .ResponseType.TOKEN
-                // ... perform additional configuration ...
-                intent.putExtra(
-                        AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,
-                        configurationBuilder.build());
-                startActivityForResult(intent, APP_REQUEST_CODE);
             }
 
 
         });
-/*
 
-        TermsConditions = (TextView) findViewById(R.id.TermsConditionId);
 
-        TermsConditions.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent termsConditionsIntent = new Intent(MainActivity.this,TermsConditions.class);
-                startActivity(termsConditionsIntent);
-            }
-        });
-*/
     }
+
+    public void phoneLogin(View view) {
+        final Intent intent = new Intent(MainActivity.this, AccountKitActivity.class);
+        AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+                new AccountKitConfiguration.AccountKitConfigurationBuilder(
+                        LoginType.PHONE,
+                        AccountKitActivity.ResponseType.CODE); // or .ResponseType.TOKEN
+        // ... perform additional configuration ...
+        intent.putExtra(
+                AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,
+                configurationBuilder.build());
+        startActivityForResult(intent, APP_REQUEST_CODE);
+    }
+
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == APP_REQUEST_CODE) {
+            goToProfileInActivity();
+
+            /*AccountKitLoginResult loginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+            String toastMessage;
+            if (loginResult.getError() != null) {
+                toastMessage = loginResult.getError().getErrorType().getMessage();
+               // goToProfileInActivity();
+            } else if (loginResult.wasCancelled()) {
+                toastMessage = "Login Cancelled";
+            } else {
+                toastMessage = "Success:" + loginResult.getAccessToken().getAccountId();
+               // goToProfileInActivity();
+            }
+
+            Toast.makeText(
+                    this,
+                    toastMessage,
+                    Toast.LENGTH_LONG)
+                    .show();*/
+        }
+    }
+
+    public  void goToProfileInActivity(){
+        Intent letsGoIntent = new Intent(MainActivity.this,HomePage.class);
+        startActivity(letsGoIntent);
+    }
+
 }
