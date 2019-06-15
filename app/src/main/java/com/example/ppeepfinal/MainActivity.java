53d37f894e2cc77a187e4346d810bdd8f097646a
System Administrator <root@MacBooks-MacBook-Air.local>
2019-06-15 13:50:36 +0600

    Facebook OTP

--- app/src/main/java/com/example/ppeepfinal/MainActivity.java
+++ app/src/main/java/com/example/ppeepfinal/MainActivity.java
@@ -7,10 +7,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
+
 public class MainActivity extends AppCompatActivity {
 
     Button LetsGoEnter, SignUpButton;
     TextView TermsConditions;
+    public static int APP_REQUEST_CODE = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +28,24 @@ public class MainActivity extends AppCompatActivity {
         LetsGoEnter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent letsGoIntent = new Intent(MainActivity.this,OTPpage.class);
-                startActivity(letsGoIntent);
+                phoneLogin(v);
+               /* Intent letsGoIntent = new Intent(MainActivity.this,OTPpage.class);
+                startActivity(letsGoIntent);*/
             }
+            public void phoneLogin(final View view) {
+                final Intent intent = new Intent(MainActivity.this, AccountKitActivity.class);
+                AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+                        new AccountKitConfiguration.AccountKitConfigurationBuilder(
+                                LoginType.PHONE,
+                                AccountKitActivity.ResponseType.CODE); // or .ResponseType.TOKEN
+                // ... perform additional configuration ...
+                intent.putExtra(
+                        AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,
+                        configurationBuilder.build());
+                startActivityForResult(intent, APP_REQUEST_CODE);
+            }
+
+
         });
 /*
 
