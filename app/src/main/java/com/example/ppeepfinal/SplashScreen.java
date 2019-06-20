7102d3f2101a68e71b9cc43130d61b840c5a4285
System Administrator <root@MacBooks-MacBook-Air.local>
2019-06-20 13:27:15 +0600

    mobile verfication

--- app/src/main/java/com/example/ppeepfinal/SplashScreen.java
+++ app/src/main/java/com/example/ppeepfinal/SplashScreen.java
@@ -5,21 +5,35 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.ppeepfinal.data.UserDatabase;
+import com.example.ppeepfinal.data.UserModel;
+
+import java.util.List;
+
 public class SplashScreen extends AppCompatActivity {
 
     private final int SPLASH_DISPLAY_LENGTH = 2500;
+    private UserDatabase mdb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        mdb = UserDatabase.getInstance(getApplicationContext());
+        final List<UserModel> user =  mdb.userDAO().loadPhone();
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
-                SplashScreen.this.startActivity(mainIntent);
-                SplashScreen.this.finish();
+                if(user.size() == 0) {
+                    Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                    SplashScreen.this.startActivity(mainIntent);
+                    SplashScreen.this.finish();
+                }else{
+                    Intent homeIntent = new Intent(SplashScreen.this, HomePage.class);
+                    SplashScreen.this.startActivity(homeIntent);
+                    SplashScreen.this.finish();
+                }
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
