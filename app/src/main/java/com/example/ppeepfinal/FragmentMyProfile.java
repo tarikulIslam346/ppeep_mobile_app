a64bbd7eac961e02a3bf54ad3502b6ab8d0c3ce9
System Administrator <root@MacBooks-MacBook-Air.local>
2019-06-20 15:18:25 +0600

    Drwaer profile name

--- app/src/main/java/com/example/ppeepfinal/FragmentMyProfile.java
+++ app/src/main/java/com/example/ppeepfinal/FragmentMyProfile.java
@@ -18,31 +18,34 @@ import java.util.List;
 
 public class FragmentMyProfile extends Fragment {
 
-
     View v;
+
     TextView myProfileName,myPhoneNo;
+
     private UserDatabase mdb;
 
-
-    public FragmentMyProfile() {
-    }
+    public FragmentMyProfile() { }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
       v=inflater.inflate(R.layout.myprofile_fragment,container,false);
-        mdb = UserDatabase.getInstance(getContext());
+
+        mdb = UserDatabase.getInstance(getContext());//intantiate room database
+
         myProfileName = v.findViewById(R.id.profile_name);
+
         myPhoneNo = v.findViewById(R.id.phone_no);
-        myProfileName.setText("Name");
-       List<UserModel> user =  mdb.userDAO().loadPhone();
-       if(user!= null && !user.equals("")){
-           //Toast.makeText(getContext(), user.get(1).getPhone(), Toast.LENGTH_SHORT).show();
-           myPhoneNo.setText(user.get(0).getPhone());
-           myProfileName.setText(user.get(0).getName());
-       }
-       //user.get()
+
+        List<UserModel> user =  mdb.userDAO().loadPhone();//select all data form room database user table
+
+        if(user.size()!= 0){//if data exist
+
+           myPhoneNo.setText(user.get(0).getPhone());// set phone to text view
+
+           myProfileName.setText(user.get(0).getName());// set name to text view
+        }
 
       return v;
 
--- app/src/main/java/com/example/ppeepfinal/HomePage.java
+++ app/src/main/java/com/example/ppeepfinal/HomePage.java
@@ -12,68 +12,80 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import com.smarteist.autoimageslider.SliderLayout;
+import com.example.ppeepfinal.data.UserDatabase;
+import com.example.ppeepfinal.data.UserModel;
+
+import java.util.List;
 
 public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
     DrawerLayout mDrawerLayout;
+
     ActionBarDrawerToggle mToggle;
 
     private TabLayout tabLayout;
+
     private ViewPager viewPager;
+
     private ViewPagerAdapter adapter;
-    //Context context;
-    private Button mPeep;
-   // SliderLayout sliderLayout;
-   // TextView profileName ;
+
+    private UserDatabase mdb;// declear databse
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
-        tabLayout = (TabLayout) findViewById(R.id.tabLayoutId);
-        viewPager = (ViewPager) findViewById(R.id.viewPagerId);
-        adapter = new ViewPagerAdapter(getSupportFragmentManager());
 
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_home_page);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayoutId);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPagerId);
+
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
 
         mDrawerLayout = findViewById(R.id.drawerId);
 
-
+        mdb = UserDatabase.getInstance(getApplicationContext());//call database here
 
         mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.nav_open,R.string.nav_close);
 
         mDrawerLayout.addDrawerListener(mToggle);
+
         mToggle.syncState();
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //get profile text_view
-        //View headerView = HomePage.f(R.layout.hea);
-       // profileName = (TextView) findViewById(R.id.profile_name);
-       //profileName.setText(" OK");
+        NavigationView navigationView = (NavigationView) findViewById(R.id.NavigationId);
 
+        View headerView = navigationView.getHeaderView(0);
 
+        TextView navUsername = (TextView) headerView.findViewById(R.id.profile_name);
 
+        List<UserModel> user =  mdb.userDAO().loadPhone();//select all data form room database user table
 
-
+        if(user.size()!=0) {navUsername.setText(user.get(0).getName());}
 
         //add fragment here
-
         adapter.AddFragment(new FragmentHome(),"Home");
+
         adapter.AddFragment(new FragmentGroup(),"My Group");
+
         //  adapter.AddFragment(new FragmentNotification() ,"Promotion");
+
         adapter.AddFragment(new FragmentNotification(),"Notification");
+
         adapter.AddFragment(new FragmentMyProfile(),"My Profile");
 
-
         viewPager.setAdapter(adapter);
+
         tabLayout.setupWithViewPager(viewPager);
 
         tabLayout.getTabAt(0).setIcon(R.drawable.icon_homelogo);
+
         tabLayout.getTabAt(1).setIcon(R.drawable.groupicon);
-
-
-
 
 /*
         for (int i = 0; i < tabLayout.getTabCount(); i++) {
