3fe882d6fbe77a3826fd1dceebf005ccfb2bdb25
P-PeeP <nayeemhasnat00@gmail.com>
2019-06-24 14:45:55 +0600

    nothing asign

--- app/src/main/java/com/example/ppeepfinal/FoodApp.java
+++ app/src/main/java/com/example/ppeepfinal/FoodApp.java
@@ -5,10 +5,12 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class FoodApp extends AppCompatActivity {
 Toolbar foodToolbar;
-
+Menu foodCart;
     private TabLayout tabLayoutId;
     private ViewPager viewPagerId;
     private ViewPagerAdapter adapter;
@@ -20,7 +22,13 @@ Toolbar foodToolbar;
         setSupportActionBar(foodToolbar);
         adapter = new ViewPagerAdapter(getSupportFragmentManager());
 
-
+   /*     @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.foodappcart, menu);
+            foodCart=menu.findItem(R.id.action_drawer_cart);
+            return super.onCreateOptionsMenu(menu);
+        }*/
 
         tabLayoutId = (TabLayout) findViewById(R.id.tabLayoutId);
         viewPagerId = (ViewPager) findViewById(R.id.viewPagerId);
@@ -42,6 +50,8 @@ Toolbar foodToolbar;
         tabLayoutId.getTabAt(1).setIcon(R.drawable.nearbyicon);
         tabLayoutId.getTabAt(2).setIcon(R.drawable.recommendedicon2);
         tabLayoutId.getTabAt(3).setIcon(R.drawable.offericon);
+
+
 
 
     }
