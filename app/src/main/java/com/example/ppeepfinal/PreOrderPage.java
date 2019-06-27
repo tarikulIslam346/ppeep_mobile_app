b6d2346975ed07c07c084b7af1ed62c6d90462e4
P-PeeP <nayeemhasnat00@gmail.com>
2019-06-27 12:36:44 +0600

    design change

---
+++ app/src/main/java/com/example/ppeepfinal/PreOrderPage.java
@@ -0,0 +1,13 @@
+package com.example.ppeepfinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreOrderPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pre_order_page);
+    }
+}
