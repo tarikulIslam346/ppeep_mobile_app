15d70b5854d991e3cbbd69f4590838ccd9b9faf0
P-PeeP <nayeemhasnat00@gmail.com>
2019-07-16 16:27:45 +0600

    order details

---
+++ app/src/main/java/com/example/ppeepfinal/OrderOnGoingList.java
@@ -0,0 +1,14 @@
+package com.example.ppeepfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderOnGoingList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_on_going_list);
+    }
+}
